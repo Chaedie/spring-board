7c3b09ab0a4889c02c21b2d03f5ef2c0051145a9
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-11 22:07:28 +0900

    Test: Auditing Test (#6)

---
+++ src/test/java/com/example/springbootboard/repository/PostRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.springbootboard.repository;
+
+import com.example.springbootboard.data.entity.Post;
+import com.example.springbootboard.data.repository.PostRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Transactional
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @DisplayName("Auditig 기능 적용 테스트")
+    void findPost() {
+        // given
+        Post post = Post.builder()
+                .postTitle("테스트 제목입니다.")
+                .postContent("테스트 내용입니다.")
+                .build();
+
+        // when
+        Post savedPost = postRepository.save(post);
+
+        // then
+        assertNotNull(savedPost.getCreatedAt());
+        assertNotNull(savedPost.getUpdatedAt());
+    }
+}
