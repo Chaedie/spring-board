479f7b00e5a04ebd85f2de157408500fbc872565
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-18 22:09:46 +0900

    Feat: 밸리데이션 에러 시 queryString추가

--- src/main/java/com/example/springbootboard/controller/UserController.java
+++ src/main/java/com/example/springbootboard/controller/UserController.java
@@ -6,6 +6,8 @@ import com.example.springbootboard.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,8 +34,12 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    public String signUpUser(UserRequestDTO userRequestDTO) {
+    public String signUpUser(@Validated UserRequestDTO userRequestDTO, Errors errors) {
+        if (errors.hasErrors()) {
+            return "redirect:login?error";
+        }
         userService.signUpUser(userRequestDTO);
+
 
         return "redirect:login";
     }
