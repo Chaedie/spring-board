6f4eed2314553fe52950bef44aa5133ec5699dd3
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-19 15:02:28 +0900

    Feat: For문을 활용한 배치 인서트 구현 (수정 필요)

---
+++ src/main/java/com/example/springbootboard/controller/Rest/v1/DummyDataController.java
@@ -0,0 +1,30 @@
+package com.example.springbootboard.controller.Rest.v1;
+
+import com.example.springbootboard.data.entity.EmailAuth;
+import com.example.springbootboard.data.repository.EmailAuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/rest/v1/dummy")
+@RequiredArgsConstructor
+public class DummyDataController {
+
+    private final EmailAuthRepository emailAuthRepository;
+    
+    @GetMapping("/insertAuthMail")
+    public String insert_100_000_AuthMail() {
+
+        for (int i = 0; i < 100_000; i++) {
+            emailAuthRepository.save(EmailAuth.builder()
+                    .userEmail(UUID.randomUUID().toString())
+                    .authCode(UUID.randomUUID().toString().substring(0, 8)).build());
+        }
+        return "done!";
+    }
+
+}
