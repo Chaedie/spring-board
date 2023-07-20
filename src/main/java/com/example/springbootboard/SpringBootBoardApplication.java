713cac32aefae21eb001f982fb3e0035f8908f9d
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-20 17:12:42 +0900

    Feat: 조회수 스케쥴링 구현
    스케쥴 설정
    JPA Auditing 설정 위치 이동 (For test)

--- src/main/java/com/example/springbootboard/SpringBootBoardApplication.java
+++ src/main/java/com/example/springbootboard/SpringBootBoardApplication.java
@@ -2,9 +2,9 @@ package com.example.springbootboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootBoardApplication {
 
