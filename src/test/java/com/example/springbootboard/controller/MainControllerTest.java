07e2559e67d3cfe8d3f730413ce3fc48de1ea38e
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-28 11:18:22 +0900

    Refactor: 테스트 코드 리팩터

--- src/test/java/com/example/springbootboard/controller/MainControllerTest.java
+++ src/test/java/com/example/springbootboard/controller/MainControllerTest.java
@@ -1,15 +1,11 @@
 package com.example.springbootboard.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MainControllerTest {
@@ -17,39 +13,30 @@ public class MainControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Autowired
-    private WebApplicationContext context;
-
-    @BeforeEach
-    public void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }
-
     @Test
     void 메인페이지_로딩() {
-        // when
+        // given
         String expectedBody = "Spring 게시판 !!!";
         String url = "/";
 
-        // then
+        // when
         String actual = restTemplate.getForObject(url, String.class);
 
+        // then
         assertThat(actual).contains(expectedBody);
     }
 
 
     @Test
     void 비회원일경우_비회원입니다_출력() {
-        // when
+        // given
         String expectedBody = "비회원입니다";
         String url = "/";
 
-        // then
+        // when
         String actual = restTemplate.getForObject(url, String.class);
 
+        // then
         assertThat(actual).contains(expectedBody);
     }
 }
