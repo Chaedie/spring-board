92f43286f58328f951ea246873709c282ce9f583
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-28 13:46:47 +0900

    Feat: CommonResonse 정의

---
+++ src/main/java/com/example/springbootboard/common/CommonResponse.java
@@ -0,0 +1,24 @@
+package com.example.springbootboard.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class CommonResponse<T> {
+    private String message;
+    private T data;
+
+    public CommonResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> CommonResponse<T> success(T data) {
+        return new CommonResponse<>("ok", data);
+    }
+
+    public static <T> ResponseEntity<CommonResponse<T>> badRequest(String message) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new CommonResponse<>(message, null));
+    }
+}
